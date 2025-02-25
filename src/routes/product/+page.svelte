4b610b6261f453d94d0a291f830fa054e6aa2df2
Mohan Reddy <mreddy@securiosity.net>
2025-02-25 10:18:19 -0800

    fix: image alignment

--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -70,7 +70,7 @@
 				</p>
 			</div>
 			<img
-				class="hidden mb-4 w-full lg:mb-0 lg:flex rounded-lg alignment mx-auto"
+				class="hidden mb-4 w-full lg:mb-0 lg:flex rounded-lg mx-auto"
 				src={productNew}
 				alt="Product Graphic"
 			/>
