4139177c0615a48b7516c79a5c743d2eb2d080f3
Mohan Reddy <mreddy@securiosity.net>
2025-02-25 18:42:28 -0800

    fix: drop-down button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -172,12 +172,12 @@
 		</p>
 
 		
-		<div
+		<p
 		<button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Watch a Demo <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
 		<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
 		</svg>
 		</button>
-		</div>
+		</p>
 
 		<!-- Dropdown menu -->
 		<div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
