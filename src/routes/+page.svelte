5cc8d47fd3b1783c1ddea6e6102da8c38838b8aa
Mohan Reddy <mreddy@securiosity.net>
2025-02-25 18:32:40 -0800

    fix: update button on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -171,7 +171,8 @@
 			when responding to ransomware, malware, phishing attacks, DNS poisoning and so much more. QSChannel™ Zones and Cloudiffusion™ enable you to deploy microsegmentation and cloud integration with unparalleled ease, security and performance.
 		</p>
 
-		<div
+		
+		
 		<button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Watch a Demo <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
 		<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
 		</svg>
@@ -193,8 +194,6 @@
 		        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Installing our Agent</a>
 		      </li>
 		    </ul>
-		</div>
-
 		
 		</div>
 
