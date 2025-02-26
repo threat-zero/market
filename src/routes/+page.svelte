37fef9a82209a5eaad4df3af04784e4fd50559fc
Mohan Reddy <mreddy@securiosity.net>
2025-02-25 18:53:24 -0800

    fix: demo button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -172,7 +172,7 @@
 		</p>
 
 		
-		
+		<!--
 		<button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Watch a Demo <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
 		<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
 		</svg>
@@ -197,8 +197,8 @@
 		    </ul>
 		
 		</div>
-
-		<!-- 
+		-->
+		 
 		<div
 			class="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16"
 		>
@@ -211,6 +211,7 @@
 				Get a Demo
 			</a>
 			
+		<!--
 			<a
 				href="https://drive.google.com/file/d/10L879kcqd5gl0iq0WfTkEXxp8vSBtJqy/view?usp=drive_link"
 				target="_blank"
@@ -219,8 +220,7 @@
 			>
 				Get our Agent
 			</a> 
-			
-			
+	
 		</div>
 		-->
 		<img
