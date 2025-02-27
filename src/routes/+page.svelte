92123954db742f49c9750ab6d0522f6302a0f4e4
Mohan Reddy <mreddy@securiosity.net>
2025-02-27 15:57:46 -0800

    fix: auto play sChannel demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -296,10 +296,9 @@
 </section>
 
 
-	<video class="w-full" controls autoplay muted>
-		<source src="https://netorgft1065679-my.sharepoint.com/:v:/g/personal/mreddy_securiosity_net/EfFViXIkPZtNj0LiDNI-1UsBkw1A76LF8nLGRfcXNhy5Ug?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=tzzB4Q" type="video/webm">
-  		Your browser does not support the video tag.
-	</video>
+<video class="w-full" controls autoplay muted>
+	<source src="https://netorgft1065679-my.sharepoint.com/:v:/g/personal/mreddy_securiosity_net/EXDREMiF81xAoPJr6yfhUQABEzBZfp9nI1ok6WYLIaRBzQ?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=1Lz4Fp" type="video/mp4">
+</video>
 
 
 <FeaturesList
