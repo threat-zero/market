1f335c3fad1d0e01a5f2fa18232e614be941daff
Mohan Reddy <mreddy@securiosity.net>
2025-03-07 11:20:10 -0800

    fix: easy policy video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -414,7 +414,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
         <div class="flex flex-col justify-center">
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Demystifying Zero Trust</h1>
+            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-3xl dark:text-white">Demystifying Zero Trust</h1>
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">ThreatMatic™ strikes the perfect balance between security and usability.</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
                 <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
@@ -429,7 +429,7 @@
             </div>
         </div>
         <div>
-            <iframe src="https://drive.google.com/file/d/1blEhLZeQBNmnO5gDXeXNAXx5u1Nogorl/preview" width="640" height="480" allow="autoplay"></iframe>
+            <iframe class="mx-auto w-full lg:max-w-xl h-64 rounded-lg sm:h-96 shadow-xl" src="https://drive.google.com/file/d/1blEhLZeQBNmnO5gDXeXNAXx5u1Nogorl/preview" allow="autoplay"></iframe>
         </div>
     </div>
 </section>
