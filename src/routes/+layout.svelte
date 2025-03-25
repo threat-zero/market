ef4a5a7ff511e972f40b43661754ceff8ea4ae33
Mohan Reddy <mreddy@securiosity.net>
2025-03-24 17:39:34 -0700

    center logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,12 +50,12 @@
 
 		<NavUl {hidden} {activeUrl} class="order-1 mr-4">
 		<img
-			class="h-20 mx-auto h-20 rounded-sm border border-gray-200 shadow-xl dark:hidden dark:border-gray-600 lg:mb-8"
+			class="h-12 mx-auto rounded-sm border border-gray-200 shadow-xl dark:hidden dark:border-gray-600 lg:mb-8"
 			src={TMbrandDark}
 			alt="tm brand"
 		/>
 		<img
-			class="h-20 mx-auto h-20 hidden rounded-sm border border-gray-200 shadow-xl dark:block dark:border-gray-600 lg:mb-8"
+			class="h-12 mx-auto hidden rounded-sm border border-gray-200 shadow-xl dark:block dark:border-gray-600 lg:mb-8"
 			src={TMbrandLight}
 			alt="tm brand dark"
 		/>
