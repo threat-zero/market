f4feeb3b973d232d3a04aa38fc41618526e31310
Mohan Reddy <mreddy@securiosity.net>
2025-04-04 16:39:06 -0700

    wider audio button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -442,7 +442,7 @@
                 <a class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white
 								rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ">
                     Learn more
-										<svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
+										<svg class="w-6 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
 										<div class="w-full">
