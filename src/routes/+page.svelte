5a95b342b0f089488d1941ef58bd4b18cc541d24
Mohan Reddy <mreddy@securiosity.net>
2025-04-04 06:44:15 -0700

    inline audio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -429,14 +429,16 @@
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">ThreatMatic™ strikes the perfect balance between security and usability.</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLScTMV_GyN480Jie4voJMwDDb6U6WUv14p0k8do3TPeeb8g9uQ/viewform?usp=sf_link" target="_blank"
-								rel="external noopener noreferrer" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+								rel="external noopener noreferrer" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white
+								rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                     Get started
                     <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
                 </a>
 
-                <a href="#" target="_blank" class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                <a href="#" target="_self" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white 
+								rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ">
                     Learn more
 										<svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
