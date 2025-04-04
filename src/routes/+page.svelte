bf495d8f489fb76ad9e8b6e763c9e2f26a4ecb14
Mohan Reddy <mreddy@securiosity.net>
2025-04-04 06:10:38 -0700

    auto play audio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -434,6 +434,12 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
                 </a>
+
+								<div>
+										<iframe class="mx-auto w-full lg:max-w-xl h-8 rounded-lg sm:h-96 shadow-xl" src="https://drive.google.com/file/d/1QW2p06boHXRGjXUbkwvHRn8Qha9kBF6t/view?usp=sharing" type="audio/wav" allow="autoplay"></iframe>
+								</div>
+
+
 								<!--
                 <a href="#" target="_blank" class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                     Learn more
