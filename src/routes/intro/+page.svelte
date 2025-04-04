72cbb6cc29ec0df097d0f329f42fabb66a369d9b
Mohan Reddy <mreddy@securiosity.net>
2025-04-03 17:37:01 -0700

    intro graphic

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import productNew from '$lib/images/product/productNew.jpg';
+	import productNew from '$lib/images/product/Background-Security Abstract.jpeg';
 	import { MetaTags } from 'svelte-meta-tags';
 </script>
 
@@ -39,31 +39,31 @@
 					Simplify Security Without Compromising Control.
 				</h2>
 				<p class="mb-8 font-light lg:text-xl">
-					ThreatMatic™ bridges the gap between security and usability. 
+					ThreatMatic™ bridges the gap between security and usability.
 
 					Our intuitive UI and modular Zero Trust architecture delivers lightning-fast, real-time protection against ransomware, malware, phishing, DNS poisoning, and more. Defend your most critical assets with unparalleled ease, security and performance..
 				</p>
 				<p class="mb-8 font-light lg:text-xl">
 					Instant Zero Trust Enforcement:
           				Block traffic in real-time with granular, rule-based policies applied to users, groups, devices, and endpoints.
-          				Audit or block applications and executables directly on endpoints. 
+          				Audit or block applications and executables directly on endpoints.
 				</p>
 				<p class="mb-8 font-light lg:text-xl">
 					Smarter Bandwidth Management:
           				Prevent network bottlenecks by managing bandwidth allocation for users, workloads, and software updates.
           				Fine-tune bandwidth at a granular level with bi-directional performance tuning.
 				</p>
-				<p class="mb-8 font-light lg:text-xl">					
+				<p class="mb-8 font-light lg:text-xl">
 					Seamless Micro-segmentation:
           				Quickly isolate malware and control host-to-host traffic to enhance performance and security.
           				Achieve secure, dynamic cloud integration with ClouDiffusion™.
-				</p>				
+				</p>
 				<p class="mb-8 font-light lg:text-xl">
 					Comprehensive Defense-in-Depth:
           				Integrated threat intelligence delivers proactive malware blocking and DNS traffic control.
           				Block malicious domains and C2 communications with precision.
 				</p>
-				
+
 			</div>
 			<img
 				class="hidden mb-4 w-full lg:mb-0 lg:flex rounded-lg"
