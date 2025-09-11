69603681b947e50f09116728c847970c8ca6458f
Mohan Reddy <mreddy@securiosity.net>
2025-09-10 17:44:10 -0700

    sChannel youtube video added

--- src/routes/schannel/+page.svelte
+++ src/routes/schannel/+page.svelte
@@ -58,7 +58,7 @@
 				      ></iframe>
 				 </div>
 
-					<--
+					<!--
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/tlfQEfB3FOs?si=IFRRT6iCMB57uKoT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 					-->
 
