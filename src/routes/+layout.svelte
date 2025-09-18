bc60a26ef0ccc362f3c09d63bb4270e3f5c570bd
Mohan Reddy <mreddy@securiosity.net>
2025-09-18 14:18:30 -0700

    update faq link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -142,10 +142,10 @@
 								<a href="/privacy" class="hover:underline">Privacy</a>
 							</li>
 							<li>
-								<a href="/faq" class="hover:underline">Terms of Use</a>
+								<a href="/terms" class="hover:underline">Terms of Use</a>
 							</li>
 							<li>
-								<a href="/terms" class="hover:underline">FAQs</a>
+								<a href="/faq" class="hover:underline">FAQs</a>
 							</li>
 						</ul>
 					</div>
