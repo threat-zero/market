778098248dff548be853d5277bbafe654086b540
Mohan Reddy <mreddy@securiosity.net>
2025-09-18 19:39:41 -0700

    FAQs

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -25,7 +25,7 @@
 	<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
 
 
-		<div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+		<div class="mx-auto max-w-screen-md font-extrabold text-gray-900 dark:text-white text-center mb-8 lg:mb-12">
 			<h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
 				FAQs
 			</h2>
