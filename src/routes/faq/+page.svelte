bd1cdf4c85b7b132f7f042e4f57f0892d1be8894
Mohan Reddy <mreddy@securiosity.net>
2025-09-18 19:27:12 -0700

    FAQs

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -30,22 +30,23 @@
 				FAQs
 			</h2>
 
-
-		<div class="collapse bg-base-100 border border-base-300">
-	  <input type="radio" name="my-accordion-1" checked="checked" />
-	  <div class="collapse-title font-semibold">How do I create an account?</div>
-	  <div class="collapse-content text-sm">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
-	</div>
-	<div class="collapse bg-base-100 border border-base-300">
-	  <input type="radio" name="my-accordion-1" />
-	  <div class="collapse-title font-semibold">I forgot my password. What should I do?</div>
-	  <div class="collapse-content text-sm">Click on "Forgot Password" on the login page and follow the instructions sent to your email.</div>
-	</div>
-	<div class="collapse bg-base-100 border border-base-300">
-	  <input type="radio" name="my-accordion-1" />
-	  <div class="collapse-title font-semibold">How do I update my profile information?</div>
-	  <div class="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
-	</div>
+			<p class="font-light text-justify text-gray-500 md:text-lg xl:text-sm dark:text-gray-400">
+						<div class="collapse bg-base-100 border border-base-300">
+					  <input type="radio" name="my-accordion-1" checked="checked" />
+					  <div class="collapse-title font-semibold">How do I create an account?</div>
+					  <div class="collapse-content text-sm">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
+				</div>
+				<div class="collapse bg-base-100 border border-base-300">
+					  <input type="radio" name="my-accordion-1" />
+					  <div class="collapse-title font-semibold">I forgot my password. What should I do?</div>
+					  <div class="collapse-content text-sm">Click on "Forgot Password" on the login page and follow the instructions sent to your email.</div>
+				</div>
+				<div class="collapse bg-base-100 border border-base-300">
+					  <input type="radio" name="my-accordion-1" />
+					  <div class="collapse-title font-semibold">How do I update my profile information?</div>
+					  <div class="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
+				</div>
+	</p>
 
  	</div>
 
