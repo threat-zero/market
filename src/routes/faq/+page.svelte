339614885ea436503b7b56234facbc7a4f04e8e7
Mohan Reddy <mreddy@securiosity.net>
2025-09-19 17:29:40 -0700

    Fixed spelling

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -27,7 +27,7 @@
 
 		<div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
 			<h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-				FAQ4
+				FAQs
 			</h2>
 		</div>
 
