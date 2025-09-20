2dbdf8fab1e03d6193787310b17b66e42b2c2066
Mohan Reddy <mreddy@securiosity.net>
2025-09-19 17:51:17 -0700

    Accordion testing

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { MetaTags } from 'svelte-meta-tags';
+	import { AccordionItem, Accordion } from 'flowbite-svelte';
 </script>
 
 <MetaTags
@@ -30,6 +31,31 @@
 				FAQs
 			</h2>
 		</div>
+
+		<Accordion>
+		  <AccordionItem>
+		    <span slot="header">My Header 1</span>
+		    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+		    <p class="text-gray-500 dark:text-gray-400">
+		      Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> get started </a>
+		      and start developing websites even faster with components on top of Tailwind CSS.
+		    </p>
+		  </AccordionItem>
+		  <AccordionItem>
+		    <span slot="header">My Header 2</span>
+		    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+		    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
+		    <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+		    <ul class="list-disc ps-5 dark:text-gray-400 text-gray-500">
+		      <li>
+		        <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> Lorem ipsum </a>
+		      </li>
+		      <li>
+		        <a href="https://tailwindui.com/" rel="noreferrer" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline"> Tailwind UI </a>
+		      </li>
+		    </ul>
+		  </AccordionItem>
+		</Accordion>
 
 		<div class="mx-auto max-w-screen-md text-center mb-4 lg:mb-4">
 				<p class="font-normal text-justify text-gray-500 md:text-lg xl:text-sm dark:text-gray-400">
