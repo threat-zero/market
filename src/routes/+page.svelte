d9bed18842b57634e32a83084d8f6ee15d2cd017
Mohan Reddy <mreddy@securiosity.net>
2025-09-25 21:03:30 -0700

    intro video placement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -408,23 +408,12 @@
 />
 
 <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-1 gap-8 lg:gap-16">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
         <div class="flex flex-col justify-center">
             <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-3xl dark:text-white">Demystifying Zero Trust</h1>
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Threatmatic™ strikes the perfect balance between security and usability.</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
-								<!--
-								<a href="https://docs.google.com/forms/d/e/1FAIpQLScTMV_GyN480Jie4voJMwDDb6U6WUv14p0k8do3TPeeb8g9uQ/viewform?usp=sf_link" target="_blank"
-								rel="external noopener noreferrer" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white
-								rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Get started
-                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
-								-->
-
-                <a class="inline-flex justify-center items-center py-3 px-5 text-base font-normal text-left text-white
+							<a class="inline-flex justify-center items-center py-3 px-5 text-base font-normal text-left text-white
 								rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ">
                     Hear from the experts
 										<svg class="w-24 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 4">
@@ -444,7 +433,7 @@
       </div>
 				<div class="aspect-video rounded-lg overflow-hidden mb-4">
 					<p
-							class="mb-8 font-light italic text-gray-500 dark:text-gray-400 sm:px-16 md:text-lg lg:text-xl lg:px-48"
+							class="mb-4 font-light italic text-gray-500 dark:text-gray-400 sm:px-16 md:text-lg lg:text-xl lg:px-48"
 						>
 							<iframe
 								src="https://drive.google.com/file/d/1Tnv4cEJge1o0CQCnQw8YJ9SDW3f20QDd/preview"
