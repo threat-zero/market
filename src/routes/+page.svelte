4587cb2dfedb7bf2affa4793897630846654b70c
Mohan Reddy <mreddy@securiosity.net>
2025-09-25 19:59:22 -0700

    Mohan intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -207,17 +207,16 @@
 			class="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16"
 		>
 
-			<div class="aspect-video rounded-lg overflow-hidden mb-4">
-				<p
-					class="mb-8 font-light text-gray-500 dark:text-gray-400 sm:px-16 md:text-lg lg:text-xl lg:px-48"
+			<p
+					class="mb-8 font-light italic text-gray-500 dark:text-gray-400 sm:px-16 md:text-lg lg:text-xl lg:px-48"
 				>
-					The Threatmatic Difference
 					<iframe
 						src="https://drive.google.com/file/d/1Tnv4cEJge1o0CQCnQw8YJ9SDW3f20QDd/preview"
 						class="w-full h-full"
 						allow=autoplay
 						>
 					</iframe>
+						The Threatmatic Difference
 				</p>
 			</div>
 
