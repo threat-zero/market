1b0e89db433193b2db22a7d0d3320dc7ec4da5e7
Mohan Reddy <mreddy@securiosity.net>
2025-09-26 16:21:03 -0700

    removed mohan videos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -409,26 +409,11 @@
 
 <section class="bg-white dark:bg-gray-900">
 
-	<!--
-		<div class="py-8 px-4 mx-auto max-w-screen-md lg:py-16 grid md:grid-cols-3 gap-8 lg:gap-16">
-
-				<div class="aspect-video rounded-lg overflow-hidden mb-4">
-					<p
-							class="mb-4 font-light italic text-gray-500 dark:text-gray-400 sm:px-16 md:text-xl lg:text-xl lg:px-48"
-						>
-							<iframe src="https://drive.google.com/file/d/1Tnv4cEJge1o0CQCnQw8YJ9SDW3f20QDd/preview"
-								allow="autoplay"
-								>
-							</iframe>
-								The Threatmatic Difference
-					</p>
-				</div>
-		-->
-
-				<div class="flex flex-col justify-center grid md:grid-cols-3">
+		<div class="py-8 px-4 mx-auto max-w-screen-md lg:py-16 grid lg:grid-cols-1 gap-8 lg:gap-16">
+				<div class="flex flex-col justify-center grid lg:grid-cols-1">
             <h1 class="mb-4 text-4xl font-semibold tracking-tight text-center text-gray-900 md:text-3xl lg:text-3xl dark:text-white">Demystifying Zero Trust</h1>
             <p class="mb-8 text-lg font-normal text-wrap text-gray-500 lg:text-xl dark:text-gray-400">Threatmatic™ strikes the perfect balance between security and usability.</p>
-
+						<!--
 						<div class="aspect-video rounded-lg overflow-hidden mb-4">
 							<p
 									class="mb-4 font-light italic text-gray-500 dark:text-gray-400 sm:px-16 md:text-xl lg:text-xl lg:px-48"
@@ -439,7 +424,7 @@
 									Why Threatmatic?
 							</p>
 						</div>
-
+						-->
 						<div class="flex flex-col justify-center space-y-4 sm:flex-row sm:space-y-0">
 							<a class="inline-flex justify-center items-center py-3 px-5 text-base font-normal text-left text-white
 								rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 ">
@@ -457,7 +442,7 @@
      								</div>
                 </a>
             </div>
-
+						<!--
 						<div class="aspect-video rounded-lg overflow-hidden mb-4">
 							<p
 									class="mb-4 font-light italic text-gray-500 dark:text-gray-400 sm:px-16 md:text-xl lg:text-xl lg:px-48"
@@ -468,6 +453,7 @@
 									Threatmatic Architecture
 								</p>
 						</div>
+						-->
     	</div>
 
 </section>
